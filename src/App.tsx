6eb5a6b514018ef7dcd8d294218bae2581c1f874
RaulSantos206 <rauljunior206@gmail.com>
2025-08-19 20:41:14 -0300

    feat: created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { Header } from "./components/header"
+
 function App() {
   return (
-   <div>
-    <h1>Revisão CP1</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error maiores eveniet autem culpa consequuntur accusantium tenetur quia modi odit explicabo, dolore dignissimos non soluta quidem nobis laudantium! Aliquid, dignissimos odit.</p>
-   </div>
+    <Header />
   )
 }
 
